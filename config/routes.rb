f36d4356033195ea40a908cadcd88d7b96885b38
Sahil Khan <sahil.khan@deeporion>
2023-07-05 11:30:50 +0530

    today first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/new'
+  # get 'books/create'
+  # get 'books/edit'
+  # get 'books/update'
+  # get 'books/destroy'
   namespace :management do
-      resources :students
-      resources :teachers
-      resources :admins
-      resources :books
-
-      root to: "admins#index"
-    end
+    resources :students
+    resources :teachers
+    resources :admins
+    resources :books
+    
+    root to: "admins#index"
+  end
   # get 'user/index
   # devise_for :teachers
   # devise_for :students
+  # resources :admins
   # devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "user#index"
